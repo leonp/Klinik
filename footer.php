acd0d9bcc9318f5f964ce47413c566a274148db0
Leon Paternoster <leon.paternoster@gmail.com>
2013-12-14 08:41:08 +0000

    Added Twitter meta box to footer
    And some typographical changes

--- footer.php
+++ footer.php
@@ -2,9 +2,13 @@
 					
 				<ul>
 			
-					<li><a href="<?php bloginfo('url'); ?>"><span itemprop="author" itemscope itemtype="http://schema.org/Person"><span itemprop="name">Leon Paternoster</span></span></a></li>
+					<li><a href="<?php bloginfo('url'); ?>"><span itemprop="author" itemscope itemtype="http://schema.org/Person"><span itemprop="name"><?php bloginfo('name'); ?></span></span></a></li>
 					<?php wp_nav_menu(array('theme_location' => 'navigation', 'container' => 'false', 'items_wrap' => '%3$s', 'depth' => '2')); ?>
-					<li><a href="http://twitter.com/leonpaternoster">Twitter</a></li>
+					<?php if ( get_the_author_meta( 'twitter' ) ) { ?>
+				
+						<li><a href="http://twitter.com/<?php the_author_meta( 'twitter' ); ?>" title="Follow <?php the_author_meta( 'display_name' ); ?> on Twitter">Twitter</a></li>
+					
+					<?php } ?>
 					<li><a href="<?php bloginfo('rss2_url'); ?>">RSS</a></li>
 				
 				</ul>
